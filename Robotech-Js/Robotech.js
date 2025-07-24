a2da4c85b8af8048bbaadef22d5ad7c5d6942e76
Hardiik-git <hardiik003@gmail.com>
2025-07-24 18:10:20 +0530

    Add Files And Code

---
+++ Robotech-Js/Robotech.js
@@ -0,0 +1,23 @@
+// Navigation Bar Section Js
+const navOpenBtn = document.querySelector(".Open-bar");
+const navCloseBtn = document.querySelector(".Close-x");
+const overLayBg = document.querySelector(".Bg-Over-lay");
+const openClassAdd = document.querySelector(".Nav-bar");
+
+// Open Navigation Bar
+navOpenBtn.addEventListener("click", () => {
+  openClassAdd.classList.add("Toggle-Open");
+  overLayBg.classList.add("Toggle-Open");
+});
+
+// Close Navigation Bar
+navCloseBtn.addEventListener("click", () => {
+  openClassAdd.classList.remove("Toggle-Open");
+  overLayBg.classList.remove("Toggle-Open");
+});
+
+// Overlay Click Close
+overLayBg.addEventListener("click", () => {
+  openClassAdd.classList.remove("Toggle-Open");
+  overLayBg.classList.remove("Toggle-Open");
+});
